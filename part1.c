63d33aa8f01087c81e63ca2326c50ca28a65b812
Noam <Noam.Leabovich@gmail.com>
2024-06-16 21:44:51 +0300

    - adding perrors

--- part1.c
+++ part1.c
@@ -32,29 +32,59 @@ int main(int argc, char const *argv[])
         {
             // second child
             int file = open("output.txt", O_CREAT | O_WRONLY | O_APPEND, 0777);
+            if (file == -1)
+            {
+                perror("open");
+                return 1;
+            }
             for (int i = 0; i < atoi(argv[4]); i++)
             {
                 printf("writing to file: %s\n", argv[2]);
                 char* buffer = concatenate(argv[2], "\n");
                 int wr = write(file, buffer, strlen(buffer));
+                if (wr == -1)
+                {
+                    perror("write");
+                    return 1;
+                }
                 free(buffer);
             }
             close(file);
+        }
+        else if (getpid() == -1)
+        {
+            perror("fork");
+            exit(1);
         }
         else
         {
             // first child
             wait(&statusFC);
             int file = open("output.txt", O_CREAT | O_WRONLY | O_APPEND, 0777);
+            if (file == -1)
+            {
+                perror("open");
+                return 1;
+            }
             for (int i = 0; i < atoi(argv[4]); i++)
             {
                 printf("writing to file: %s\n", argv[3]);
                 char* buffer = concatenate(argv[3], "\n");
                 int wr = write(file, buffer, strlen(buffer));
+                if (wr == -1)
+                {
+                    perror("write");
+                    return 1;
+                }
                 free(buffer);
             }
             close(file);
         }
+    }
+    else if (getpid() == -1)
+    {
+        perror("fork");
+        exit(1);
     }
     else
     {
@@ -62,11 +92,21 @@ int main(int argc, char const *argv[])
         int statusP;
         wait(&statusP);
         int file = open("output.txt", O_CREAT | O_WRONLY | O_APPEND, 0777);
+        if (file == -1)
+        {
+            perror("open");
+            return 1;
+        }
         for (int i = 0; i < atoi(argv[4]); i++)
         {
             printf("writing to file: %s\n", argv[1]);
             char* buffer = concatenate(argv[1], "\n");
             int wr = write(file, buffer, strlen(buffer));
+            if (wr == -1)
+            {
+                perror("write");
+                return 1;
+            }
             free(buffer);
         }
         close(file);
