904205a0481ffb7643289c9ca1e28c8161913568
Noam <Noam.Leabovich@gmail.com>
2024-06-18 23:59:42 +0300

    - adding doc the part1 and part2

--- part1.c
+++ part1.c
@@ -5,21 +5,32 @@
 #include <fcntl.h>
 #include <string.h>
 
-char* concatenate(const char* str1, const char* str2) {
-    char* result = malloc(strlen(str1) + strlen(str2) + 1); // +1 for the null-terminator
-    if (result == NULL) {
+// a function to concatenate two strings
+char *concatenate(const char *str1, const char *str2)
+{
+    // +1 for the null-terminator
+    char *result = malloc(strlen(str1) + strlen(str2) + 1);
+    // result will be null if malloc fails we indicate so
+    if (result == NULL)
+    {
         perror("malloc");
         exit(1);
     }
+
+    // copy str1 to result
     strcpy(result, str1);
+    // concatenate str2 to result
     strcat(result, str2);
+    // return the result
     return result;
 }
 
 int main(int argc, char const *argv[])
 {
+    // check if the number of arguments is correct
     if (argc != 5)
     {
+        // print the usage of the program
         fprintf(stderr, "Usage: %s <parent_message> <child1_message> <child2_message> <count>", argv[0]);
         return 1;
     }
@@ -31,56 +42,81 @@ int main(int argc, char const *argv[])
         if (fork() == 0)
         {
             // second child
+            // wecreate the file
             int file = open("output.txt", O_CREAT | O_WRONLY | O_APPEND, 0777);
+            // check if the file is created successfully
             if (file == -1)
             {
+                // print an error message
                 perror("open");
                 return 1;
             }
+            // write the message to the file
             for (int i = 0; i < atoi(argv[4]); i++)
             {
-                char* buffer = concatenate(argv[2], "\n");
+                // concatenate the message with a newline character
+                char *buffer = concatenate(argv[2], "\n");
+                // write the message to the file
                 int wr = write(file, buffer, strlen(buffer));
+                // check if the write is successful
                 if (wr == -1)
                 {
+                    // print an error message
                     perror("write");
                     return 1;
                 }
+                // free the buffer
                 free(buffer);
             }
+            // close the file
             close(file);
         }
+        // check if the fork failed
         else if (getpid() == -1)
         {
+            // print an error message
             perror("fork");
             exit(1);
         }
         else
         {
             // first child
+            // wait for the second child to finish
             wait(&statusFC);
+            // open the file
             int file = open("output.txt", O_CREAT | O_WRONLY | O_APPEND, 0777);
+            // check if the file is opened successfully
             if (file == -1)
             {
+                // print an error message
                 perror("open");
                 return 1;
             }
+            // write the message to the file
             for (int i = 0; i < atoi(argv[4]); i++)
             {
-                char* buffer = concatenate(argv[3], "\n");
+                // concatenate the message with a newline character
+                char *buffer = concatenate(argv[3], "\n");
+                // write the message to the file
                 int wr = write(file, buffer, strlen(buffer));
+                // check if the write is successful
                 if (wr == -1)
                 {
+                    // print an error message
                     perror("write");
                     return 1;
                 }
+                // free the buffer
                 free(buffer);
             }
+            // close the file
             close(file);
         }
     }
+    // check if the fork failed
     else if (getpid() == -1)
     {
+        // print an error message
         perror("fork");
         exit(1);
     }
@@ -88,25 +124,37 @@ int main(int argc, char const *argv[])
     {
         // parent
         int statusP;
+        // wait for the first child to finish
         wait(&statusP);
+        // open the file
         int file = open("output.txt", O_CREAT | O_WRONLY | O_APPEND, 0777);
+        // check if the file is opened successfully
         if (file == -1)
         {
+            // print an error message
             perror("open");
             return 1;
         }
+        // write the message to the file
         for (int i = 0; i < atoi(argv[4]); i++)
         {
-            char* buffer = concatenate(argv[1], "\n");
+            // concatenate the message with a newline character
+            char *buffer = concatenate(argv[1], "\n");
+            // write the message to the file
             int wr = write(file, buffer, strlen(buffer));
+            // check if the write is successful
             if (wr == -1)
             {
+                // print an error message
                 perror("write");
                 return 1;
             }
+            // free the buffer
             free(buffer);
         }
+        // close the file
         close(file);
     }
+
     return 0;
 }
--- part2.c
+++ part2.c
@@ -4,8 +4,10 @@
 #include <sys/wait.h>
 #include <fcntl.h>
 
+// the file that will be used as a lock
 const char *lock = "lockfile.lock";
 
+// a function to write a message to the standard output
 void write_message(const char *message, int count)
 {
     for (int i = 0; i < count; i++)
@@ -15,18 +17,27 @@ void write_message(const char *message, int count)
     }
 }
 
+// a function to lock the lock file
 void locking_lock()
 {
+    /*
+    try to open the lock file with the O_CREAT and O_EXCL flags which will
+    create the file if it doesn't exist otherwise it will fail
+    */
+
     while (open(lock, O_CREAT | O_EXCL, 0644) == -1)
     {
         usleep(1000); // Wait for 1ms before retrying to acquire the lock
     }
 }
 
+// a function to unlock the lock file
 void unlocking_lock()
 {
+    // remove the lock file
     if (remove(lock) != 0)
     {
+        // print an error message if the file is not removed successfully
         perror("Error deleting lock file");
         exit(1);
     }
@@ -34,42 +45,67 @@ void unlocking_lock()
 
 int main(int argc, char const *argv[])
 {
+    // check if the number of arguments is correct
     if (argc < 4)
     {
         fprintf(stderr, "Usage: %s <message1> <message2> ... <count>", argv[0]);
         return 1;
     }
 
+    // the number of forks is the number of arguments - 2
     int numOfForks = argc - 2;
+    // the number of prints is the last argument
     int numOfPrints = atoi(argv[argc - 1]);
 
+    // an array to store the children's pids
     pid_t *children = (pid_t *)malloc(sizeof(pid_t) * numOfForks);
 
+    // check if malloc is successful
+    if (children == NULL)
+    {
+        perror("malloc");
+        exit(1);
+    }
+
+    // create the children
     for (int i = 0; i < numOfForks; i++)
     {
+        // create a new child
         pid_t newChild = fork();
         if (newChild == 0)
         {
             // child process
+
+            // locking the lock and preventing other children from writing
             locking_lock();
+            // write the message
             write_message(argv[i + 1], numOfPrints);
+            // release the lock
             unlocking_lock();
+            // exit the child
             exit(0);
         }
+        // check if the fork is successful
         else if (newChild < 0)
         {
+            // print an error message
             perror("fork");
             exit(1);
         }
         else
+            // parent process
+
+            // store the child's pid
             children[i] = newChild;
     }
 
+    // parent will wait for all children to finish
     for (int i = 0; i < numOfForks; i++)
     {
         waitpid(children[i], NULL, 0);
     }
 
+    // free the children array
     free(children);
 
     return 0;
